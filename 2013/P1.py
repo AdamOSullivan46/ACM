3c45a41783c292741b237e7a55c91075e4e59cf2
AdamOSullivan46 <AdamOSullivan46@users.noreply.github.com>
2015-03-25 17:10:29 +0000

    Create P1.py

---
+++ 2013/P1.py
@@ -0,0 +1,15 @@
+# 2013 Problem 1
+# Sum Free
+
+def Check(lst):
+    for i, num1 in enumerate(lst):
+        for j, num2 in enumerate(lst):
+            if i == j:
+                continue
+            if num1+num2 in lst:
+                return 0
+    return 1
+
+N = int(input())
+lst = [int(i) for i in input().split()]
+print(Check(lst))
